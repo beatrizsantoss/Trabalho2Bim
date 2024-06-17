94185c7233fa0fc8801efad1cbe9a4521fe59c4e
Beatriz <BEATRIZ.CRUZ84@ETEC.SP.GOV.BR>
2024-06-17 17:24:28 +0000

    save1

--- Trabalho/app/(tabs)/index.tsx
+++ Trabalho/app/(tabs)/index.tsx
@@ -1,5 +1,7 @@
 import {View, Image, StyleSheet, Platform } from 'react-native';
+import React from 'react'
 
+import Home from '../../components/home'
 
 export default function HomeScreen() {
   return (
